464f37066124a06722d907fed09f9f2c36ca4379
GooseOb <gooose.ob@gmail.com>
2023-08-05 16:41:57 +0300

    rename function inside test lib [skip ci]

--- test/lib.js
+++ test/lib.js
@@ -1,8 +1,8 @@
 export const print = (label, msg, colorCode = '0') => {
 	console.log(`\x1b[${colorCode}m[${label}]\x1b[0m ${msg}`);
 };
-const fail = (testName, msg) => {
-	print('fail', `${testName}: ${msg}`, '31');
+const failed = (testName, msg) => {
+	print('failed', `${testName}: ${msg}`, '31');
 	process.exit(1);
 };
 const passed = (testName) => {
@@ -13,9 +13,9 @@ export const test = (name, fn, cases) => {
 	for (const [input, expectedValue] of cases) {
 		const output = fn(input);
 		if (output !== expectedValue)
-			fail(
+			failed(
 				name,
-				`\ninput: ${input}\noutput: ${output}\nexpected: ${expectedValue}`
+				`\ninput: ${input}\noutput: ${output}\nexpected: ${expectedValue}`,
 			);
 	}
 	passed(name);
