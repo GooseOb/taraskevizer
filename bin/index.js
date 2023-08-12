0e14cccf634c8113001c5ec3367ca08554a790e4
GooseOb <gooose.ob@gmail.com>
2023-08-12 12:01:30 +0300

    improve CLI [skip ci]

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,81 @@
 #!/usr/bin/env node
 
 const print = (...msgs) => {
-	console.log('[taraskevizer]', ...msgs);
+	console.log('\x1b[34m[taraskevizer]\x1b[0m', ...msgs);
 };
 
-if (process.argv[2] === '-v' || process.argv[2] === '--version') {
+process.argv.shift();
+process.argv.shift();
+
+const checkForOptions = (options) => {
+	for (const option of options)
+		if (process.argv[0] === option) {
+			process.argv.shift();
+			return true;
+		}
+	return false;
+};
+
+if (checkForOptions(['-v', '--version'])) {
 	print(require('../package.json').version);
 	process.exit(0);
 }
 
-const { taraskSync } = require('../dist');
-const text = process.argv.splice(2).join(' ');
+const stgs = {
+	html: {},
+};
 
-print(taraskSync(text));
+const arr = [
+	[
+		['--latin', '-l'],
+		() => {
+			stgs.abc = 1;
+		},
+	],
+	[
+		['--arabic', '-a'],
+		() => {
+			stgs.abc = 2;
+		},
+	],
+	[
+		['--jrandom', '-jr'],
+		() => {
+			stgs.j = 1;
+		},
+	],
+	[
+		['--jalways', '-ja'],
+		() => {
+			stgs.j = 2;
+		},
+	],
+	[
+		['--g', '-g'],
+		() => {
+			stgs.j = 2;
+		},
+	],
+];
+
+opnionsEater: while (true) {
+	for (const [options, callback] of arr)
+		if (checkForOptions(options)) {
+			callback();
+			continue opnionsEater;
+		}
+	break;
+}
+
+const text = process.argv.join(' ');
+if (!text) process.exit(0);
+
+const { taraskSync } = require('../dist');
+
+const htmlTagsToNodeColors = (text) =>
+	text
+		.replace(/<tarF>/g, '\x1b[32m')
+		.replace(/<tar[LH].*?>/g, '\x1b[35m')
+		.replace(/<\/\S+>/g, '\x1b[0m');
+
+print(htmlTagsToNodeColors(taraskSync(text, stgs)));
