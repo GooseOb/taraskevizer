1de56401adefd1105bffba7005b53a487ddb9195
GooseOb <gooose.ob@gmail.com>
2024-02-25 00:56:54 +0100

    [skip ci] small change

--- build-config/json-generator.ts
+++ build-config/json-generator.ts
@@ -1,20 +1,7 @@
 import * as path from 'path';
 import { writeFile, mkdir } from 'node:fs/promises';
-import {
-	wordlist,
-	softeners,
-	latinLetters,
-	latinLettersUpperCase,
-	gobj,
-	greekLetters,
-	greekLettersUpperCase,
-	thWords,
-} from '../src/dict';
-import type { Dict } from '../src/types';
+import * as dict from '../src/dict';
 const outputPath = path.resolve('json');
-
-const dictToStr = (dict: Dict) =>
-	JSON.stringify(dict.map(([pattern, result]) => [pattern.source, result]));
 
 export default () =>
 	mkdir(outputPath)
@@ -24,14 +11,23 @@ export default () =>
 		.then(() =>
 			Promise.all(
 				[
-					['wordlist', dictToStr(wordlist)],
-					['softeners', dictToStr(softeners)],
-					['latinLetters', dictToStr(latinLetters)],
-					['latinLettersUpperCase', dictToStr(latinLettersUpperCase)],
-					['greekLetters', dictToStr(greekLetters)],
-					['greekLettersUpperCase', dictToStr(greekLettersUpperCase)],
-					['thWords', dictToStr(thWords)],
-					['g', JSON.stringify(gobj)],
+					...(
+						[
+							'wordlist',
+							'softeners',
+							'latinLetters',
+							'latinLettersUpperCase',
+							'greekLetters',
+							'greekLettersUpperCase',
+							'thWords',
+						] satisfies (keyof typeof dict)[]
+					).map((name) => [
+						name,
+						JSON.stringify(
+							dict[name].map(([pattern, result]) => [pattern.source, result])
+						),
+					]),
+					['g', JSON.stringify(dict.gobj)],
 				].map(([fileName, str]) =>
 					writeFile(path.resolve(outputPath, fileName + '.json'), str)
 				)
