7d144b92d97469c55566da92e4c3ac6cfa45e051
GooseOb <gooose.ob@gmail.com>
2024-04-14 12:14:53 +0200

    replace `else` block by `else if`

--- src/bin.ts
+++ src/bin.ts
@@ -116,17 +116,15 @@ while ((currOption = process.argv.shift())) {
 let text = '';
 if (process.argv.length) {
 	text = process.argv.join(' ');
+} else if (process.stdin.isTTY) {
+	text = await readline
+		.createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		})
+		.question(prefix + 'Enter the text:\n');
 } else {
-	if (process.stdin.isTTY) {
-		text = await readline
-			.createInterface({
-				input: process.stdin,
-				output: process.stdout,
-			})
-			.question(prefix + 'Enter the text:\n');
-	} else {
-		for await (const chunk of process.stdin) text += chunk;
-	}
+	for await (const chunk of process.stdin) text += chunk;
 }
 
 const taraskevizer = new Taraskevizer({ general, html, nonHtml });
