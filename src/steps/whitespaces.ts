9b96ebc0f88812d2c9c591f9bffd02f52f240842
GooseOb <gooose.ob@gmail.com>
2024-06-01 03:51:08 +0200

    optimize whitespace escaping again

--- src/steps/whitespaces.ts
+++ src/steps/whitespaces.ts
@@ -1,48 +1,22 @@
 import type { TaraskStep } from './types';
 
-type WhiteSpaceStorage = { spaces: string[]; positions: number[] };
+type WhiteSpaceStorage = { spaces: string[] };
 
 export const whitespacesToSpaces: TaraskStep<WhiteSpaceStorage> = (
 	text,
 	{ storage }
 ) => {
 	storage.spaces = [];
-	storage.positions = [];
-	let result = '';
-	let i = 0;
-
-	while (i < text.length) {
-		if (/\s/.test(text[i])) {
-			let start = i;
-			while (i < text.length && /\s/.test(text[i])) ++i;
-			storage.spaces.push(text.slice(start, i));
-			storage.positions.push(result.length);
-			result += ' ';
-		} else {
-			result += text[i];
-			++i;
-		}
-	}
-
-	return result;
+	return text.replace(/\s+/g, (match) => {
+		storage.spaces.push(match);
+		return ' ';
+	});
 };
 
 export const restoreWhitespaces: TaraskStep<WhiteSpaceStorage> = (
 	text,
 	{ storage }
 ) => {
-	let result = '';
-	let lastIndex = 0;
-	let spaceIndex = 0;
-
-	for (let i = 0; i < text.length; i++) {
-		if (text[i] === ' ' && spaceIndex < storage.spaces.length) {
-			result += text.slice(lastIndex, i) + storage.spaces[spaceIndex];
-			lastIndex = i + 1;
-			++spaceIndex;
-		}
-	}
-
-	result += text.slice(lastIndex);
-	return result;
+	storage.spaces.reverse();
+	return text.replace(/ /g, () => storage.spaces.pop()!);
 };
