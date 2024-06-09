96b0a920a2209dbf56f2e47f1ecc58b933ff7aec
GooseOb <gooose.ob@gmail.com>
2024-06-09 03:50:21 +0200

    small improvement

--- src/bin.ts
+++ src/bin.ts
@@ -7,7 +7,6 @@ import {
 	tarask,
 	pipelines,
 } from './index.js';
-import type { NonHtmlOptions, TaraskOptions, HtmlOptions } from './types';
 declare const __CLI_HELP__: string;
 declare const __VERSION__: string;
 
@@ -31,12 +30,11 @@ if (checkForOptions(['-h', '--help'])) {
 	process.exit(0);
 }
 
-const general: Partial<TaraskOptions> = {};
-const nonHtml: Partial<NonHtmlOptions> = {
-	variations: 2,
-	ansiColors: true,
-};
-const html: Partial<HtmlOptions> = { g: true };
+const cfg = new TaraskConfig({
+	general: {},
+	html: { g: true },
+	nonHtml: { variations: VARIATION.ALL, ansiColors: true },
+});
 
 type Mode = keyof typeof pipelines;
 let mode: Mode = 'plainText';
@@ -55,62 +53,62 @@ const optionDict = toHashTable([
 	[
 		['--latin', '-l'],
 		() => {
-			general.abc = dicts.alphabets.latin;
+			cfg.general.abc = dicts.alphabets.latin;
 		},
 	],
 	[
 		['--latin-ji', '-lj'],
 		() => {
-			general.abc = dicts.alphabets.latinJi;
+			cfg.general.abc = dicts.alphabets.latinJi;
 		},
 	],
 	[
 		['--arabic', '-a'],
 		() => {
-			general.abc = dicts.alphabets.arabic;
+			cfg.general.abc = dicts.alphabets.arabic;
 		},
 	],
 	[
 		['--jrandom', '-jr'],
 		() => {
-			general.j = REPLACE_J.RANDOM;
+			cfg.general.j = REPLACE_J.RANDOM;
 		},
 	],
 	[
 		['--jalways', '-ja'],
 		() => {
-			general.j = REPLACE_J.ALWAYS;
+			cfg.general.j = REPLACE_J.ALWAYS;
 		},
 	],
 	[
 		['--no-escape-caps', '-nec'],
 		() => {
-			general.doEscapeCapitalized = false;
+			cfg.general.doEscapeCapitalized = false;
 		},
 	],
 	[
 		['--h'],
 		() => {
-			nonHtml.h = true;
-			html.g = false;
+			cfg.nonHtml.h = true;
+			cfg.html.g = false;
 		},
 	],
 	[
 		['--no-variations', '-nv'],
 		() => {
-			nonHtml.variations = VARIATION.NO;
+			cfg.nonHtml.variations = VARIATION.NO;
 		},
 	],
 	[
 		['--first-variation-only', '-fvo'],
 		() => {
-			nonHtml.variations = VARIATION.FIRST;
+			cfg.nonHtml.variations = VARIATION.FIRST;
 		},
 	],
 	[
 		['--no-color', '-nc'],
 		() => {
-			nonHtml.ansiColors = false;
+			cfg.nonHtml.ansiColors = false;
 		},
 	],
 	[
@@ -162,8 +160,6 @@ if (process.argv.length) {
 
 	text = Buffer.concat(chunks, length).toString();
 }
-
-const cfg = new TaraskConfig({ general, html, nonHtml });
 
 if (process.stdout.write(tarask(text, pipelines[mode], cfg) + '\n')) {
 	process.exit(0);
