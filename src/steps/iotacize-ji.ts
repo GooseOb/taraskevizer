fb89a7d0a025142986bd6b8b9de339000ad1de14
GooseOb <gooose.ob@gmail.com>
2024-06-20 21:58:01 +0200

    improve iotacizing ji

--- src/steps/iotacize-ji.ts
+++ src/steps/iotacize-ji.ts
@@ -4,6 +4,7 @@ import { dictFrom, mutatingStep, replaceWithDict } from '../lib/index';
 const iDict = dictFrom.raw([
 	[/([аеёіоуыэюя] )і ў/, '$1й у'],
 	[/([аеёіоуыэюя] )і /, '$1й '],
+	[/([аеёіоуыэюя] ?)і/, '$1йі'],
 	[` і(?=${iwords})`, ' йі'],
 ]);
 
