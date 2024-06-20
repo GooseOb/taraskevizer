671ef0dea72da9bfc971998a2558e16c50d2f507
GooseOb <gooose.ob@gmail.com>
2024-06-21 01:34:18 +0200

    simplify softening

--- src/lib/soften.ts
+++ src/lib/soften.ts
@@ -2,10 +2,8 @@ import { softeners } from '../dict/softening';
 import { replaceWithDict } from './replace-with-dict';
 
 export const soften = (text: string) => {
-	softening: do {
+	do {
 		text = replaceWithDict(text, softeners);
-		for (const [pattern, result] of softeners)
-			if (result !== '$1дзьдз' && pattern.test(text)) continue softening;
-		return text;
-	} while (true);
+	} while (softeners.some(([pattern]) => pattern.test(text)));
+	return text;
 };
