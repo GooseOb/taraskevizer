ef914309aff973f495b757cf0173ce837fd77a76
GooseOb <gooose.ob@gmail.com>
2024-08-13 16:29:58 +0200

    [skip ci] improve `mutatingStep` docs

--- src/lib/mutating-step.ts
+++ src/lib/mutating-step.ts
@@ -5,6 +5,19 @@ import type { TaraskStep } from '../steps/types';
  *
  * > Not recommended to use if
  * a step doesn't ALWAYS change the text.
+ *
+ * @example
+ * type TextWrapperStorage = { wrapText: (text: string) => string };
+ *
+ * const trimStep = mutatingStep<TextWrapperStorage>(
+ *   ({ text, storage: { wrapText } }) => wrapText(text.trim())
+ * );
+ * // is equivalent to
+ * const trimStep: TaraskStep<TextWrapperStorage> = (options) => {
+ *   options.text = options.storage.wrapText(
+ *     options.text.trim()
+ *   );
+ * };
  */
 export const mutatingStep =
 	<T extends object = {}>(
