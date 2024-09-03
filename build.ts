9a6cc4489b29a0fec850e876db85a7e79d41bf52
GooseOb <gooose.ob@gmail.com>
2024-09-03 03:36:03 +0200

    [skip ci] use a custom build script instead of tsc

---
+++ build.ts
@@ -0,0 +1,142 @@
+import {
+	createProgram,
+	getPreEmitDiagnostics,
+	flattenDiagnosticMessageText,
+	readConfigFile,
+	parseJsonConfigFileContent,
+	sys,
+	createCompilerHost,
+} from 'typescript';
+import { readdirSync, lstatSync } from 'fs';
+import { readFile, writeFile } from 'fs/promises';
+import { join, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const getPrinter =
+	<T extends { write: (s: string) => void }>(writer: T, clr = '33') =>
+	(msg: string) =>
+		writer.write(`\x1b[${clr}m[build]\x1b[0m ${msg}\n`);
+const printErr = getPrinter(process.stderr, '31');
+const print = getPrinter(process.stdout);
+const printWithTime = (msg: string) => {
+	const time = performance.now();
+	process.stdout.write(
+		`\x1b[33m[${(time - lastTime).toFixed(2)}ms]\x1b[0m ${msg}\n`
+	);
+	lastTime = time;
+};
+
+print('Starting build process...');
+
+const startTime = performance.now();
+let lastTime = startTime;
+let exitCode = 0;
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const parseConfig = (dir: string, file = 'tsconfig.json') => {
+	const absoluteDir = join(__dirname, dir);
+	return parseJsonConfigFileContent(
+		readConfigFile(join(absoluteDir, file), sys.readFile).config,
+		sys,
+		absoluteDir
+	).options;
+};
+
+const compilerOptions = {
+	...parseConfig('.'),
+	...parseConfig('src'),
+};
+
+printWithTime('Compiler options parsed');
+
+const srcFiles = readdirSync('src', {
+	recursive: true,
+	withFileTypes: true,
+}).filter((file) => file.isFile() && file.name.endsWith('.ts'));
+
+printWithTime('Source file paths read');
+
+const program = createProgram(
+	srcFiles.map(({ parentPath, name }) => join(parentPath, name)),
+	compilerOptions
+);
+
+printWithTime('Program created');
+
+const emitResult = program.emit();
+
+if (emitResult.emitSkipped) {
+	printErr('File emitting was skipped');
+	exitCode = 1;
+} else {
+	printWithTime('Files emited');
+}
+
+const allDiagnostics = getPreEmitDiagnostics(program).concat(
+	emitResult.diagnostics
+);
+for (const diagnostic of allDiagnostics) {
+	if (diagnostic.file) {
+		const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
+			diagnostic.start!
+		);
+		const message = flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+		print(
+			`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
+		);
+	} else {
+		console.log(flattenDiagnosticMessageText(diagnostic.messageText, '\n'));
+	}
+}
+
+printWithTime('Diagnostics printed');
+
+await Promise.all(
+	srcFiles.map((file) => {
+		const parentPath = file.parentPath.replace(/^src/, 'dist');
+		const fileName = join(
+			__dirname,
+			parentPath,
+			file.name.replace(/ts$/, 'js')
+		);
+		return readFile(fileName, 'utf8').then((content) =>
+			writeFile(
+				fileName,
+				content
+					.replace(/\/\*.*?\*\//gs, '')
+					.replace(
+						/((?:im|ex)port.+from\s+["'])(.+)(?<!\.js)(?=["'];)/g,
+						(_$0, $1, $2) => {
+							try {
+								if (lstatSync(resolve(parentPath, $2)).isDirectory()) {
+									$2 += '/index';
+								}
+							} catch {}
+							return $1 + $2 + '.js';
+						}
+					)
+			)
+		);
+	})
+);
+
+printWithTime('Imports fixed');
+
+await Promise.all([
+	readFile('./dist/bin.js', 'utf8'),
+	readFile('./package.json', 'utf8'),
+	readFile('./cli-help.txt', 'utf8'),
+]).then(([content, pjson, cliHelp]) =>
+	writeFile(
+		'dist/bin.js',
+		content
+			.replace(/__VERSION__/g, `"${JSON.parse(pjson).version}"`)
+			.replace(/__CLI_HELP__/g, `\`${cliHelp}\``)
+	)
+);
+
+printWithTime('Version and help text injected');
+
+print(`Done in ${(performance.now() - startTime).toFixed(2)}ms`);
+process.exit(exitCode);
