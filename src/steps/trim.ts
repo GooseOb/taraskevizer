e847911605fc8bf3d88f21317d230b6935994a91
GooseOb <gooose.ob@gmail.com>
2024-09-16 19:21:38 +0200

    update trim storage structure

--- src/steps/trim.ts
+++ src/steps/trim.ts
@@ -6,16 +6,20 @@ import { mutatingStep } from '../lib';
  * Used in {@link untrim}.
  */
 export type TrimStorage = {
-	beforeTrim: string;
-	afterTrim: string;
+	trim: {
+		before: string;
+		after: string;
+	};
 };
 
 /**
  * Captures whitespaces around the text.
  */
 export const trim = mutatingStep<TrimStorage>(({ text, storage }) => {
-	storage.beforeTrim = /^\s*/.exec(text)![0];
-	storage.afterTrim = /\s*$/.exec(text)![0];
+	storage.trim = {
+		before: /^\s*/.exec(text)![0],
+		after: /\s*$/.exec(text)![0],
+	};
 	return ` ${text.trim()} `;
 });
 
@@ -23,5 +27,10 @@ export const trim = mutatingStep<TrimStorage>(({ text, storage }) => {
  * Brings original whitespaces around the text back.
  */
 export const untrim = mutatingStep<TrimStorage>(
-	({ text, storage }) => storage.beforeTrim + text.trim() + storage.afterTrim
+	({
+		text,
+		storage: {
+			trim: { before, after },
+		},
+	}) => before + text.trim() + after
 );
