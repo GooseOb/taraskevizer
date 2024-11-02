a20116f94fdaf817f7acd724f6b2fdc010c5b592
GooseOb <gooose.ob@gmail.com>
2024-11-02 04:25:49 +0100

    delete constants

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ export * from './config';
 export * as steps from './steps';
 export * as pipelines from './pipelines';
 export * as lib from './lib';
-export * from './constants';
 export * as wrappers from './wrappers';
