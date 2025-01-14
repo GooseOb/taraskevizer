05e7f3a23e5e87ef4a5fd57d0733308f275f397c
GooseOb <gooose.ob@gmail.com>
2025-01-14 15:59:01 +0100

    update tag interaction

--- src/html-tag-interactions.ts
+++ src/html-tag-interactions.ts
@@ -1,15 +1,96 @@
 import { gobj } from './dict';
+import type { Wrappers } from './wrappers';
 
 type ChangeableElement = HTMLElement & { seqNum: number };
 
-const applyG = (el: ChangeableElement) => {
+const applyG = (el: Element) => {
 	el.textContent = gobj[el.textContent as keyof typeof gobj];
 };
 
 type NodeList<T extends Node> = T[] | NodeListOf<T>;
 
+export const initInteractiveTags = ({
+	variable,
+	letterH,
+}: Record<keyof Wrappers, string>) => {
+	const changeList: number[] = [];
+
+	return {
+		update: (root: Element) => {
+			const elems = root.querySelectorAll<ChangeableElement>(
+				`${letterH}, ${variable}`
+			);
+			if (changeList.length > elems.length) {
+				changeList.length = elems.length;
+			} else {
+				while (changeList.length < elems.length) {
+					changeList.push(0);
+				}
+			}
+			for (let i = 0; i < changeList.length; i++) {
+				const el = elems[i];
+				el.seqNum = i;
+				if (changeList[i]) {
+					switch (el.tagName) {
+						case letterH:
+							{
+								applyG(el);
+							}
+							break;
+						case variable: {
+							let data = el.dataset.l!;
+							if (data.includes(',')) {
+								const dataArr = data.split(',');
+								data = el.innerHTML;
+								for (let j = 0; j < changeList[i]; ++j) {
+									dataArr.push(data);
+									data = dataArr.shift()!;
+								}
+								el.dataset.l = dataArr.join(',');
+							} else {
+								el.dataset.l = el.innerHTML;
+							}
+							el.innerHTML = data;
+						}
+					}
+				}
+			}
+		},
+		tryAlternate: (el: Element | ChangeableElement) => {
+			if ('seqNum' in el) {
+				switch (el.tagName) {
+					case letterH:
+						{
+							changeList[el.seqNum] = changeList[el.seqNum] ? 0 : 1;
+							applyG(el);
+						}
+						break;
+					case variable: {
+						let data = el.dataset.l!;
+						if (data.includes(',')) {
+							const dataArr = data.split(',');
+							dataArr.push(el.innerHTML);
+							data = dataArr.shift()!;
+							changeList[el.seqNum] =
+								(changeList[el.seqNum] + 1) % (dataArr.length + 1);
+							el.dataset.l = dataArr.join(',');
+						} else {
+							changeList[el.seqNum] = changeList[el.seqNum] ? 0 : 1;
+							el.dataset.l = el.innerHTML;
+						}
+						el.innerHTML = data;
+					}
+				}
+			}
+		},
+	};
+};
+
 const changeList: number[] = [];
 
+/*
+ * @deprecated
+ */
 export const apply = (elems: NodeList<ChangeableElement>) => {
 	if (changeList.length > elems.length) {
 		changeList.length = elems.length;
@@ -50,6 +131,7 @@ export const apply = (elems: NodeList<ChangeableElement>) => {
 
 /**
  * @param el non-changeable elements are ignored
+ * @deprecated
  **/
 export const tryAlternate = (el: Element | ChangeableElement) => {
 	if ('seqNum' in el) {
