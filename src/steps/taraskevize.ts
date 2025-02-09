74c556e88747ed18cb7fdc4ab3739b087987861a
GooseOb <gooose.ob@gmail.com>
2025-02-09 18:28:26 +0100

    add words from GooseOb/taraskevizatar#15 (before 585)

--- src/steps/taraskevize.ts
+++ src/steps/taraskevize.ts
@@ -1,6 +1,7 @@
 import { wordlist } from '@/dict';
 import { endZSoftenAndNiaBiaz, mutatingStep, soften } from '@/lib';
+import { dict } from '@/lib/debug';
 
 export const taraskevize = mutatingStep(({ text }) =>
-	endZSoftenAndNiaBiaz(soften(wordlist(text)))
+	endZSoftenAndNiaBiaz(soften(dict(wordlist, /меля/)(text)))
 );
