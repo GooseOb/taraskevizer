f57f46a749fa765481abb30a8bb487d44e1c8daa
GooseOb <gooose.ob@gmail.com>
2025-12-11 15:45:38 +0100

    [skip ci] use var instead of const in worker code

--- src/bin/worker.js
+++ src/bin/worker.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
-const {
+var {
 	parentPort,
 	workerData: { argv, dirname },
 } = require('node:worker_threads');
-const { resolve } = require('node:path');
-const { pipelines } = require(resolve(dirname, '..'));
-const { parseArgs } = require(resolve(dirname, 'parse-args'));
+var { resolve } = require('node:path');
+var { pipelines } = require(resolve(dirname, '..'));
+var { parseArgs } = require(resolve(dirname, 'parse-args'));
 
-const { mode, cfg } = parseArgs(argv);
+var { mode, cfg } = parseArgs(argv);
 
 parentPort.on('message', (chunk) => {
 	parentPort.postMessage(pipelines[mode](chunk, cfg));
